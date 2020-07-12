12fba92108aff6e320f94ce9b2b36354424e1974
neel1998 <neellovepainting@gmail.com>
2020-07-12 15:31:24 +0530

    Make apk script added

---
+++ make_apk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mainPath=/home/neel/AndroidStudioProjects/TemplateCustomerApp
+
+p=$mainPath/app/src/main/assets/meta_data.txt
+#echo $p
+echo $1 > $p
+
+appName='2s|.*|<string name="app_name">'$2'</string>|'
+
+sed -i "$appName" $mainPath/app/src/main/res/values/strings.xml
+
+id='7s/.*/        applicationId "com.abc.'$2'"/'
+sed -i "$id" $mainPath/app/build.gradle
+
+cd $mainPath
+./gradlew assembleDebug
+cd -
+
+mv $mainPath/app/build/outputs/apk/debug/app-debug.apk $mainPath/app/build/outputs/apk/debug/$2.apk
